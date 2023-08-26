e24eed3a19c559c6544ed612df26a712eaa65601
Kyle Roth <kylrth@gmail.com>
2023-08-26 17:12:26 +0000

    improve error logging

--- internal/server/utils.go
+++ internal/server/utils.go
@@ -3,9 +3,21 @@ package server
 import (
 	"net/http"
 
+	"github.com/cobaltspeech/log"
 	"github.com/gofiber/fiber/v2"
 )
 
-func serverError(c *fiber.Ctx, err string) error {
-	return c.Status(http.StatusInternalServerError).SendString(err)
+// HiddenError signifies that an error should be logged but not reported to the client.
+type HiddenErr struct {
+	error
+}
+
+func serverError(l log.Logger, c *fiber.Ctx, msg string, err error) error {
+	l.Error("msg", "internal server error", "message", msg, "error", err)
+
+	if _, ok := err.(HiddenErr); !ok { //nolint:errorlint // just checking top error type
+		msg += ": " + err.Error()
+	}
+
+	return c.Status(http.StatusInternalServerError).SendString(msg)
 }
