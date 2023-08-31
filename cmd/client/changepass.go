d491c55a5c9ca1ebef9ae1af71943f555e39edd0
Kyle Roth <kylrth@gmail.com>
2023-08-31 14:31:47 +0000

    client changepass command

---
+++ cmd/client/changepass.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/cobaltspeech/log"
+	"github.com/kylrth/disco-bouncer/pkg/client"
+	"github.com/spf13/cobra"
+)
+
+var changePassCmd = &cobra.Command{
+	Use:   "changepass",
+	Short: "Change your password",
+	Args:  cobra.NoArgs,
+	Run:   withLAndC(changePass),
+}
+
+func changePass(_ log.Logger, c *client.Client, _ []string) error {
+	// prompt for password
+	fmt.Fprint(os.Stderr, "New password: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if !scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return fmt.Errorf("read password: %w", err)
+		}
+
+		return errors.New("no password entered")
+	}
+	password := scanner.Text()
+
+	return c.Admin.ChangePassword(context.Background(), os.Getenv("BOUNCER_PASS"), password)
+}
